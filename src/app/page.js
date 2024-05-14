51464270250b40d485befe05a61ff2483d9502e1
Abhishek <abhishekvishwakarma3244@gmail.com>
2024-05-14 11:33:29 +0530

    changes

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
+import ProductCard from "./components/ProductCard";
 const page = () => {
   return (
     <>
       <div>
         <Navbar />
+        <ProductCard/>
         <Footer/>
       </div>
     </>
